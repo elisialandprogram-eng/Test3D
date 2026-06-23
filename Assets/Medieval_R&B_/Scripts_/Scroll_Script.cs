bfe7ecbc32b0c2e85156d01091b6461cb8e45c5f
Elliot 9 <61218648-elliot912@users.noreply.replit.com>
2026-06-23 08:50:27 +0000

    Add

---
+++ Assets/Medieval_R&B_/Scripts_/Scroll_Script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scripts_ {
+
+    public class Scroll_Script : MonoBehaviour {
+        [SerializeField] private List<Sprite> spriteList;
+        [SerializeField] private GameObject panel_B_;
+        public GameObject prefabImage;
+
+        void Start() {
+            foreach (Sprite sprite in spriteList) {
+                GameObject newObject = Instantiate(prefabImage, panel_B_.transform);
+                newObject.SetActive(true);
+                Image newImage = newObject.GetComponent<Image>();
+
+                // Asigna el sprite como imagen en el componente Image
+                newImage.sprite = sprite;
+            }
+        }
+    }
+}
